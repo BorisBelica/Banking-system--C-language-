223ece0868f7d53b92cf37acb2049a823258d99c
Boris <boris.belica2@gmail.com>
2022-11-15 15:37:09 +0100

    ver. 0.1.1
    - registration data

--- main.c
+++ main.c
@@ -1,34 +1,58 @@
 #include <stdio.h>
 #include <unistd.h>		// pause() function
 
+
 #define SIZE 100
 
 	
-// function prototypes	
-int mainMenu();
+// FUNCTION PROTOTYPES
+int userMenu(void);
+void welcomeMenu(void);
+int generateRandomID(void);
+void flushNewLine(void);
 
-// struct
+void registerNewAccount(void);
+void loginToAccount(void);
+void forgotPassword(void);
+
+
+// STRUCTURES
+struct Account {
+	int ID;
+	double balance;
+};
+
 struct Person {
-	int personID;
+	int ID;
+	
 	char firstName[SIZE];
 	char secondName[SIZE];
 	int dayOfBirth, monthOfBirth, yearOfBirth;
-	char cityOfBirth[SIZE];
+	
 	char IDcardNumber[SIZE];
+	char address[SIZE];
+	
+	char email[SIZE];
+	char password[SIZE];
+	
+	struct Account account;
 };
 
 // main function
 int main () {
 	
-	// global variables
-	int menuChoice = 0;
-	double accBalance = 0;
-	double amount = 0;
+	// GLOBAL VARIABLES
+	struct Person arrayOfPersons[SIZE];
+
+	// 1. Start welcome menu for login/register/forgot password
+	welcomeMenu();
 	
+	
+	/*
 	// infinte menu loop
 	while (1) {
 		
-		menuChoice = mainMenu();
+		menuChoice = userMenu();
 		
 		switch (menuChoice) {
 			
@@ -77,16 +101,26 @@ int main () {
 				exit(0);
 		}
 	}
+	*/
 	
 	return 0;
 }
 
-int mainMenu () {
+int generateRandomID (void) {
+	int randomID = 0;
+	
+	srand((unsigned int)time(NULL));   // Initialization, should only be called once.
+	randomID = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+	
+	return randomID;
+}
+
+int userMenu () {
 	
 		int choice = 0;
 	
 		system("clear");
-		printf("\n==== MAIN MENU ===\n\n");
+		printf("\n==== USER MAIN MENU ===\n\n");
 		
 		printf("[1] Deposit money\n");
 		printf("[2] Withdraw money\n");
@@ -98,6 +132,141 @@ int mainMenu () {
 		scanf("%d", &choice);
 		
 		return choice;
+}
+
+void welcomeMenu (void) {
+	
+		int choice;
+		
+		system("clear");
+		printf("==== WELCOME TO BANKING SYSTEM ===\n\n");
+		
+		printf("[1] Create new account\n");
+		printf("[2] Login\n");
+		printf("[3] Forgot password\n");
+		printf("[4] Exit\n");
+		
+		// Choice of user
+		do {
+			printf("\nChoose menu option [1 - 4]: ");
+			scanf("%d", &choice);
+			flushNewLine();
+		} while (choice < 1 || choice > 4 );
+	
+		
+		switch (choice) {
+			case 1:
+				registerNewAccount();
+				break;
+				
+			case 2:
+				loginToAccount();
+				break;
+			
+			case 3:
+				forgotPassword();
+				break;		
+		}
+		return;
+}
+
+void takeInput(char ch[SIZE]) {
+	fgets(ch, SIZE, stdin);
+	ch[strlen(ch) - 1] = 0;
+}
+
+void registerNewAccount(void) {
+	system("clear");
+	
+	// LOCAL VARIABLES
+	FILE *fp;
+	struct Person person;
+	char confirmChar;
+
+	//GETTING DATA PERSONAL DATA
+	
+	system("clear");
+	printf("[CREATE NEW ACCOUNT]\n\n");
+	person.ID = generateRandomID();
+	
+	printf("Enter your name:\t");
+	fgets(person.firstName, sizeof(person.firstName), stdin);
+	person.firstName[strcspn(person.firstName, "\n")] = 0;		// flush new line
+	
+	printf("Enter your surname:\t");
+	fgets(person.secondName, sizeof(person.secondName), stdin);
+	person.secondName[strcspn(person.secondName, "\n")] = 0; 	// flush new line
+	
+	printf("Enter your e-mail address:\t");
+	fgets(person.email, sizeof(person.email), stdin);
+	person.email[strcspn(person.email, "\n")] = 0;				// flush new line
+	
+	printf("Enter your day of birth:\t");
+	scanf("%d", &person.dayOfBirth);
+	flushNewLine();
+	
+	printf("Enter your month of birth:\t");
+	scanf("%d", &person.monthOfBirth);
+	flushNewLine();
+	
+	printf("Enter your year of birth:\t");
+	scanf(" %d", &person.yearOfBirth);
+	flushNewLine();
+	
+	printf("Enter your address:\t");
+	fgets(person.address, sizeof(person.address), stdin);
+	person.address[strcspn(person.address, "\n")] = 0;			// flush new line
+	
+	printf("Enter your password:\t");
+	fgets(person.password, sizeof(person.password), stdin);
+	person.password[strcspn(person.password, "\n")] = 0;		// flush new line
+	
+	//print data to confirm correctness
+	system("clear");
+	printf("\n--PERSON INFO TO SAVE--\n");
+	printf("\nID: %d", person.ID);
+	printf("\nName: %s %s", person.firstName, person.secondName);
+	printf("\nE-mail: %s", person.email);
+	printf("\nBirth: %d/%d/%d", person.dayOfBirth, person.monthOfBirth, person.yearOfBirth);
+	printf("\nAddress: %s", person.address);
+	printf("\nPassword: %s", person.password);
+
+	printf("\n\n--CONFIRM DATA? [y/n]--\n");
+	
+	while (1) {
+		scanf("%c",&confirmChar);
+		confirmChar = tolower(confirmChar);
+		if (confirmChar == 'y' || confirmChar == 'n') {
+			break;
+		}
+	}
+
+	// data not correct, repeat register form
+	if (confirmChar == 'n') {
+		printf("\nPlease enter your data again...\n\n");
+		sleep(2);
+		flushNewLine();
+		registerNewAccount();
+	}
+	
+	// data confirmed save it
+	else {
+		printf("\n\nSaving data to file");
+	}		
+	
+}
+
+void flushNewLine(void)
+{
+	while(getchar() != '\n');
+}
+
+void loginToAccount (void) {
+	printf("Login account function");
+}
+
+void forgotPassword (void) {
+	printf("Forgot password function");
 }
 
 
